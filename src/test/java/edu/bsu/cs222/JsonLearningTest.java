93e39fb50807436d2aaaae3bb1ccc420289b5c5d
Yanni Su <ysu@bsu.edu>
2019-09-17 16:20:53 -0400

    Add testCountRevisions in JsonLearningTest.java

--- src/test/java/edu/bsu/cs222/JsonLearningTest.java
+++ src/test/java/edu/bsu/cs222/JsonLearningTest.java
@@ -1,4 +1,22 @@
 package edu.bsu.cs222;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class JsonLearningTest {
+
+    @Test
+    public void testCountRevisions (){
+        JsonParser parser = new JsonParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
+        Reader reader = new InputStreamReader(inputStream);
+        JsonElement rootElement = parser.parse(reader);
+        Assert.assertNotNull(rootElement);
+    }
 }
