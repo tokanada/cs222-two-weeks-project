46513e04639fc77fd1352594aca6e1e3828249fd
YanniSu <ysu@bsu.edu>
2019-09-17 17:17:59 -0400

    Finish testCountRevisions in JsonLearningTest.java

--- src/test/java/edu/bsu/cs222/JsonLearningTest.java
+++ src/test/java/edu/bsu/cs222/JsonLearningTest.java
@@ -1,6 +1,8 @@
 package edu.bsu.cs222;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +10,7 @@ import org.junit.Test;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Map;
 
 public class JsonLearningTest {
 
@@ -17,6 +20,16 @@ public class JsonLearningTest {
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
         Reader reader = new InputStreamReader(inputStream);
         JsonElement rootElement = parser.parse(reader);
-        Assert.assertNotNull(rootElement);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+        JsonArray array = null;
+
+        for (Map.Entry<String,JsonElement> entry : pages.entrySet()) {
+            JsonObject entryObject = entry.getValue().getAsJsonObject();
+            array = entryObject.getAsJsonArray("revisions");
+
+        }
+        Assert.assertEquals(4, array.size());
+
     }
 }
