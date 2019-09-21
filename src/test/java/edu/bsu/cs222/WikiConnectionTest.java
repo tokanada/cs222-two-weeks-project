869b45aaef2c007f43e5e1505af78586c42e63e1
tokanada <davidt.3200@gmail.com>
2019-09-21 18:38:42 -0400

    Add connectToWikipedia test

--- src/test/java/edu/bsu/cs222/WikiConnectionTest.java
+++ src/test/java/edu/bsu/cs222/WikiConnectionTest.java
@@ -2,11 +2,36 @@ package edu.bsu.cs222;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URLConnection;
+import java.util.List;
 
 public class WikiConnectionTest {
     @Test
     public void testIsOnline() {
         WikiConnection wikiConnection = new WikiConnection();
         Assert.assertTrue(wikiConnection.isOnline());
+    }
+
+    @Test
+    public void testConnectToWikipedia() {
+        WikiConnection wikiConnection = new WikiConnection();
+        Assert.assertNotNull(wikiConnection.connectToWikipedia("youtube"));
+    }
+
+    @Test
+    public void testSearchWikipediaHasResults() throws Exception{
+        RevisionParser parser = new RevisionParser();
+        String searchTerm = "Zappa";
+        String encodedSearch = WikiURLEncoder.encode(searchTerm);
+        URLConnection connection = WikiConnection.connectToWikipedia(encodedSearch);
+        InputStream inputStream = connection.getInputStream();
+        List<Revision> revisionList = parser.parse(inputStream);
+        System.out.println(revisionList.get(0).getUser());
+        Assert.assertEquals(revisionList.size(), 24);
     }
 }
