2a4d43db27836bc0b6d38f4d096b96aa2fd722c0
tokanada <davidt.3200@gmail.com>
2019-09-21 18:01:17 -0400

    Complete WikiURLEncoder

--- src/main/java/edu/bsu/cs222/WikiURLEncoder.java
+++ src/main/java/edu/bsu/cs222/WikiURLEncoder.java
@@ -1,7 +1,15 @@
 package edu.bsu.cs222;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class WikiURLEncoder {
     public static String encode(String inputString) {
-        return "Frank+Zappa";
+        try{
+            return URLEncoder.encode(inputString, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e){
+            throw new RuntimeException(e.getCause());
+        }
     }
 }
