b3c8d84d0a92aa44c93d88d66839b075bb0f5d34
tokanada <davidt.3200@gmail.com>
2019-09-21 17:26:26 -0400

    Fix RevisionParser
    RevisionParser now correctly separates user and timestamp elements from JsonArray. Anonymous users are also accounted for.

--- src/test/java/edu/bsu/cs222/ParserTest.java
+++ src/test/java/edu/bsu/cs222/ParserTest.java
@@ -14,10 +14,41 @@ import java.util.List;
 
 public class ParserTest {
     @Test
-    public void testParse() throws IOException, SAXException {
+    public void testParsable() throws IOException, SAXException {
         RevisionParser parser = new RevisionParser();
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
         List<Revision> revisions = parser.parse(inputStream);
-        Assert.assertEquals("RL0919", revisions.get(1));
+        Assert.assertNotNull(revisions);
+    }
+    @Test
+    public void testParseGetUser() throws IOException, SAXException {
+        RevisionParser parser = new RevisionParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
+        List<Revision> revisions = parser.parse(inputStream);
+        Assert.assertEquals("RL0919", revisions.get(0).getUser());
+    }
+
+    @Test
+    public void testParseGetTimestamp() throws IOException, SAXException {
+        RevisionParser parser = new RevisionParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
+        List<Revision> revisions = parser.parse(inputStream);
+        Assert.assertEquals("2019-09-14T21:14:54Z", revisions.get(0).getTimestamp());
+    }
+
+    @Test
+    public void testParseIsNotAnonymous() throws  IOException, SAXException {
+        RevisionParser parser = new RevisionParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
+        List<Revision> revisions = parser.parse(inputStream);
+        Assert.assertEquals(false, revisions.get(0).isAnonymous());
+    }
+
+    @Test
+    public void testParseIsAnonymous() throws IOException, SAXException {
+        RevisionParser parser = new RevisionParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
+        List<Revision> revisions = parser.parse(inputStream);
+        Assert.assertEquals(true, revisions.get(3).isAnonymous());
     }
 }
---
+++ src/test/java/edu/bsu/cs222/RevisionTest.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs222;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RevisionTest {
+    @Test
+    public void testRevision() {
+        Revision revision = new Revision("User", "2019-09-14T21:14:54Z", false);
+        Assert.assertEquals("User", revision.getUser());
+        Assert.assertEquals("2019-09-14T21:14:54Z", revision.getTimestamp());
+        Assert.assertEquals(false, revision.isAnonymous());
+    }
+}
