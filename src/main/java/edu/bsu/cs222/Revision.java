729a629c1b0d206b927e8fbefd56543fcd43d73a
tokanada <davidt.3200@gmail.com>
2019-09-21 17:24:50 -0400

    Implement Revision Object
    getUser(), getTimestamp(), isAnonymous() methods added.
    
    Default Constructor method implemented with instance variables.

--- src/main/java/edu/bsu/cs222/Revision.java
+++ src/main/java/edu/bsu/cs222/Revision.java
@@ -1,4 +1,25 @@
 package edu.bsu.cs222;
 
 public class Revision {
+    private String user;
+    private String timestamp;
+    private boolean anonymous;
+
+    public Revision(String userElement, String timestampElement, boolean anonymous) {
+        this.user = userElement;
+        this.timestamp = timestampElement;
+        this.anonymous = anonymous;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public boolean isAnonymous() {
+        return anonymous;
+    }
 }
