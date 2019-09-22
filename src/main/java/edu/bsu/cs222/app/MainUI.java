b71ad26e2d377dd63c860f4372eef15e942f9c41
tokanada <davidt.3200@gmail.com>
2019-09-22 16:43:22 -0400

    Add basic functionality to MainUI

--- src/main/java/edu/bsu/cs222/app/MainUI.java
+++ src/main/java/edu/bsu/cs222/app/MainUI.java
@@ -2,6 +2,8 @@ package edu.bsu.cs222.app;
 
 import edu.bsu.cs222.Revision;
 import edu.bsu.cs222.RevisionParser;
+import edu.bsu.cs222.WikiConnection;
+import edu.bsu.cs222.WikiURLEncoder;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
@@ -12,7 +14,9 @@ import org.xml.sax.SAXException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -24,28 +28,56 @@ public class MainUI extends Application {
     }
 
     private Scene createScene() {
-        final TextArea jsonArea = new TextArea();
-        final Button button = new Button("Parse");
-        final TextField outputField = new TextField();
+        final TextField jsonArea = new TextField();
+        final Button button = new Button("Search");
+        final TextArea outputField = new TextArea();
         outputField.setEditable(false);
         button.setOnAction(event -> {
-            RevisionParser parser = new RevisionParser();
-            InputStream textBoxInput = new ByteArrayInputStream(jsonArea.getText().getBytes(StandardCharsets.UTF_8));
-            List<Revision> list = null;
-            try {
-                list = parser.parse(textBoxInput);
-            } catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.WARNING);
-                alert.setContentText(e.getMessage());
-                alert.show();
-            }
-            outputField.setText("There were " + list.size() + " revisions found");
+            List<Revision> revisionList = new ArrayList<>();
+            String textBoxInput = jsonArea.getText();
+            if(!isConnectedToInternet()){
+
+            };
+            revisionList = retrieveRevisionList(revisionList, textBoxInput);
+            outputField.setText(printList(revisionList));
         });
         return new Scene(new VBox(
-                new Label("Enter your JSON below"),
+                new Label("Enter your Search Term below"),
                 jsonArea,
                 button,
                 outputField));
     }
+
+    private boolean isConnectedToInternet() {
+        WikiConnection wikiConnection = new WikiConnection();
+        boolean onlineCheck = wikiConnection.isOnline();
+        return onlineCheck;
+    }
+
+    private List<Revision> retrieveRevisionList(List<Revision> revisionList, String inputString) {
+        try {
+            RevisionParser parser = new RevisionParser();
+            String encodedSearch = WikiURLEncoder.encode(inputString);
+            URLConnection urlConnection = WikiConnection.connectToWikipedia(encodedSearch);
+            InputStream inputStream = urlConnection.getInputStream();
+            revisionList = parser.parse(inputStream);
+            return revisionList;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private String printList(List<Revision> revisionList) {
+        String outputString = "";
+        for (Revision revision : revisionList){
+            outputString += "User: " + revision.getUser() + "\nDate: " + revision.getTimestamp() + "\n\n";
+        }
+        if(revisionList == null) {
+            return null;
+        }
+        return outputString;
+    }
+
+
 }
 
