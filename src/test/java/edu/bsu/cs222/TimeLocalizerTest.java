7b470c5373f8e1eac7f23f4dab4c640490561047
tokanada <davidt.3200@gmail.com>
2019-09-22 16:43:46 -0400

    Add TimeLocalizerTest

---
+++ src/test/java/edu/bsu/cs222/TimeLocalizerTest.java
@@ -0,0 +1,12 @@
+package edu.bsu.cs222;
+
+import org.junit.Test;
+
+public class TimeLocalizerTest {
+    @Test
+    public void testStringToDateConversion() {
+        TimeLocalizer timeLocalizer = new TimeLocalizer();
+        Date date = timeLocalizer.convertToDate("2019-09-14T08:14:03Z");
+
+    }
+}
