4b911607053ab8a926f21bfb04f260fee28b3cdc
tokanada <davidt.3200@gmail.com>
2019-09-22 16:44:08 -0400

    Update WikiConnection

--- src/main/java/edu/bsu/cs222/WikiConnection.java
+++ src/main/java/edu/bsu/cs222/WikiConnection.java
@@ -1,9 +1,5 @@
 package edu.bsu.cs222;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
