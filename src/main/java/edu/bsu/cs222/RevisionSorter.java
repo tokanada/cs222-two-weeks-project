2d3f18073e64fc8ce1ba93588d47fcce5c33cbd4
tokanada <davidt.3200@gmail.com>
2019-09-25 21:49:12 -0400

    Implement Sort By Grouping Implement Sort By Grouping Tests

--- src/main/java/edu/bsu/cs222/RevisionSorter.java
+++ src/main/java/edu/bsu/cs222/RevisionSorter.java
@@ -1,39 +1,60 @@
 package edu.bsu.cs222;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class RevisionSorter {
+class RevisionSorter {
 
 
-    public List<Revision> sortMostRevisions(List<Revision> unsortedList) {
+    List<Revision> sortByMostRevisions(List<Revision> unsortedList) {
+        try {
+            List<String> usernameList = populateList(unsortedList);
+            Map<String, Integer> unsortedMap = new HashMap<>();
+            countOccurrences(usernameList, unsortedMap);
+            HashMap sortedMap = sortMap(unsortedMap);
+            return sortRevisionList(sortedMap, unsortedList);
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
 
-        List<String> usernameList = populateList(unsortedList);
-        Map<String, Integer> unsortedMap = new HashMap<>();
-        unsortedMap = countOccurrences(usernameList, unsortedMap);
-        Map<String, Integer> sortedMap = sortMap(unsortedMap);
-        List<Revision> sortedRevisionList = sortRevisionList(sortedMap, unsortedList);
-
-        return sortedRevisionList;
+    List<Revision> groupByIP(List<Revision> ungroupedList) {
+        List<Revision> anonList = new ArrayList<>();
+        List<Revision> sortedList = sortByMostRevisions(ungroupedList);
+        int sortedListSize = sortedList.size();
+        for(int i = 0; i < sortedListSize; i++) {
+            separateAnonymous(sortedList, anonList);
+        }
+        sortedList.addAll(anonList);
+        return sortedList;
     }
 
     private List<Revision> sortRevisionList(Map<String, Integer> sortedMap, List<Revision> unsortedList) {
         List<Revision> sortedList = new ArrayList<>();
-
         List<String> usernameList = getUserList(sortedMap);
-
         for(String username : usernameList) {
-            for(Revision revision : unsortedList) {
-                if(username.equals(revision.getUser())){
-                    sortedList.add(revision);
-                    unsortedList.remove(revision);
-                    break;
-                }
-            }
-
+            compareRevisionLists(username, unsortedList, sortedList);
         }
-
         return sortedList;
+    }
+
+    private void separateAnonymous(List<Revision> sortedList, List<Revision> anonList) {
+        for (Revision revision : sortedList) {
+            if (revision.isAnonymous()) {
+                anonList.add(revision);
+                sortedList.remove(revision);
+                break;
+            }
+        }
+    }
+
+    private void compareRevisionLists(String username, List<Revision> unsortedList, List<Revision> sortedList) {
+        for(Revision revision : unsortedList) {
+            if(username.equals(revision.getUser())){
+                sortedList.add(revision);
+                unsortedList.remove(revision);
+                break;
+            }
+        }
     }
 
     private List<String> getUserList(Map<String, Integer> userMap) {
@@ -47,20 +68,17 @@ public class RevisionSorter {
     }
 
     private HashMap sortMap(Map<String, Integer> unsortedMap) {
-        List linkedList = new LinkedList(unsortedMap.entrySet());
-
+        List<Object> linkedList = new LinkedList<>(unsortedMap.entrySet());
         Collections.sort(linkedList, new Comparator() {
             public int compare(Object firstValue, Object secondValue) {
                 return ((Comparable) ((Map.Entry) (secondValue)).getValue())
                         .compareTo(((Map.Entry) (firstValue)).getValue());
             }
         });
-
         HashMap sortedHashMap = new LinkedHashMap();
-        for (Iterator iterator = linkedList.iterator(); iterator.hasNext();) {
-            Map.Entry entry = (Map.Entry) iterator.next();
+        for (Object o : linkedList) {
+            Map.Entry entry = (Map.Entry) o;
             sortedHashMap.put(entry.getKey(), entry.getValue());
-            System.out.println(entry.getKey());
         }
         return sortedHashMap;
     }
@@ -73,16 +91,13 @@ public class RevisionSorter {
         return stringList;
     }
 
-    private Map<String, Integer> countOccurrences(List<String> stringList, Map<String, Integer> frequencyMap) {
+    private void countOccurrences(List<String> stringList, Map<String, Integer> frequencyMap) {
         for(String username : stringList){
             Integer count = frequencyMap.get(username);
             if (count == null){
                 count = 0;
             }
-
             frequencyMap.put(username, count + 1);
         }
-
-        return frequencyMap;
     }
 }
--- src/test/java/edu/bsu/cs222/RevisionSorterTest.java
+++ src/test/java/edu/bsu/cs222/RevisionSorterTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.List;
 
 public class RevisionSorterTest {
@@ -16,40 +15,57 @@ public class RevisionSorterTest {
         RevisionParser parser = new RevisionParser();
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
         List<Revision> unsortedList = parser.parse(inputStream);
-        List<Revision> sortedList = sorter.sortMostRevisions(unsortedList);
-
-        String outputString = "";
-        for (Revision revision : sortedList){
-            outputString += "User: " + revision.getUser() + "\nDate: " + revision.getTimestamp() + "\n\n";
-        }
-
-        System.out.println(outputString);
-
+        List<Revision> sortedList = sorter.sortByMostRevisions(unsortedList);
         Assert.assertNotEquals(sortedList, unsortedList);
     }
 
     @Test
-    public void testRevisionSortPullFromWikipedia() {
+    public void testRevisionGroupSort() {
         RevisionSorter sorter = new RevisionSorter();
-        List<Revision> unsortedList = new ArrayList<>();
+        RevisionParser parser = new RevisionParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
+        List<Revision> unsortedList = parser.parse(inputStream);
+        List<Revision> sortedList = sorter.sortByMostRevisions(unsortedList);
+        inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
+        unsortedList = parser.parse(inputStream);
+        List<Revision> groupedList = sorter.groupByIP(unsortedList);
+        Assert.assertNotEquals(groupedList, sortedList);
+    }
+
+    @Test
+    public void testRevisionSortPullFromWikipedia() {
         try {
+            RevisionSorter sorter = new RevisionSorter();
             RevisionParser parser = new RevisionParser();
-            String encodedSearch = WikiURLEncoder.encode("Zappa");
+            String encodedSearch = WikiURLEncoder.encode("zappa");
             URLConnection urlConnection = WikiConnection.connectToWikipedia(encodedSearch);
             InputStream inputStream = urlConnection.getInputStream();
-            unsortedList = parser.parse(inputStream);
+            List<Revision> unsortedList = parser.parse(inputStream);
+            List<Revision> sortedList = sorter.sortByMostRevisions(unsortedList);
+            Assert.assertNotEquals(sortedList, unsortedList);
         } catch (IOException e) {
             throw new RuntimeException(e.getCause());
         }
-        List<Revision> sortedList = sorter.sortMostRevisions(unsortedList);
+    }
 
-        String outputString = "";
-        for (Revision revision : sortedList){
-            outputString += "User: " + revision.getUser() + "\nDate: " + revision.getTimestamp() + "\n\n";
+    @Test
+    public void testRevisionGroupPullFromWikipedia() {
+        try {
+            RevisionSorter sorter = new RevisionSorter();
+            RevisionParser parser = new RevisionParser();
+
+            String encodedSearch = WikiURLEncoder.encode("zappa");
+            URLConnection urlConnection = WikiConnection.connectToWikipedia(encodedSearch);
+
+            InputStream inputStream = urlConnection.getInputStream();
+            List<Revision> unsortedList = parser.parse(inputStream);
+            List<Revision> sortedList = sorter.sortByMostRevisions(unsortedList);
+
+            inputStream = getClass().getClassLoader().getResourceAsStream("WikipediaApiResults.json");
+            unsortedList = parser.parse(inputStream);            List<Revision> groupedList = sorter.groupByIP(unsortedList);
+            Assert.assertNotEquals(sortedList, groupedList);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getCause());
         }
-
-        System.out.println(outputString);
-
-        Assert.assertNotEquals(sortedList, unsortedList);
     }
 }
